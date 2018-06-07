7b8cdfe682423c2161bd5495fc477091a3938dc5
Dizzi <brandimax2@gmail.com>
2018-06-07 15:13:53 +0200

    add texture

--- BossFight/map.js
+++ BossFight/map.js
@@ -1,15 +1,16 @@
+
+
 var mapInit = function (scene) {
 
-    if (BABYLON.Engine.isSupported()) {
-        var sun = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(60, 100, 10), scene);
+    var sun = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(60, 100, 10), scene);
 
-        var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
-        var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
-        extraGroundMaterial.diffuseTexture = new BABYLON.Texture("ground.jpg", scene);
-        extraGroundMaterial.diffuseTexture.uScale = 60;
-        extraGroundMaterial.diffuseTexture.vScale = 60;
-        extraGround.position.y = -2.05;
-        extraGround.material = extraGroundMaterial;
+    var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
+    var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
+    extraGroundMaterial.diffuseTexture = new BABYLON.Texture("Resources/map/greybrickwall000.png", scene);
+    extraGroundMaterial.diffuseTexture.uScale = 60;
+    extraGroundMaterial.diffuseTexture.vScale = 60;
+    extraGround.position.y = -2.05;
+    extraGround.material = extraGroundMaterial;
 
-    }
+
 };
---
+++ README.md
@@ -0,0 +1,3 @@
+# Links 
+- [map](https://www.eternalcoding.com/?p=263)
+- [textures](https://opengameart.org/textures)
