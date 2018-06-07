3fbc0de3b7fa4a44da2163c43e8feefe0200862d
Eric Stefan Miele <ericstefan.miele@gmail.com>
2018-06-07 19:32:27 +0200

    camera follows warrior and translation with WASD

--- BossFight/character.js
+++ BossFight/character.js
@@ -1,14 +1,17 @@
-var initCharacter = function (scene, shadowGenerator) {
+var initCharacter = function (scene, camera, shadowGenerator) {
 
+    var warrior = 0;
     BABYLON.SceneLoader.ImportMesh("", "Models/psc-warrior-babylon/", "psc-warrior.babylon", scene, function (newMeshes) {
-        var warrior = newMeshes[0];
+        warrior = newMeshes[0];
+        camera.target = warrior;
 
         warrior.scaling = new BABYLON.Vector3(3, 3, 3);
         warrior.rotation.y = Math.PI;
         warrior.position = new BABYLON.Vector3(0, -3, 0);
 
-        shadowGenerator.getShadowMap().renderList.push(warrior);
+        // warrior.physicsImpostor = new BABYLON.PhysicsImpostor(s, BABYLON.PhysicsImpostor.SphereImpostor, {mass: 1});
+
+        // shadowGenerator.getShadowMap().renderList.push(warrior);
 
     });
-
 };
--- BossFight/main.js
+++ BossFight/main.js
@@ -2,18 +2,20 @@ window.onload = function init() {
 
     if (!BABYLON.Engine.isSupported()) return;
 
-    BABYLON.OBJFileLoader.OPTIMIZE_WITH_UV = true;
+    // BABYLON.OBJFileLoader.OPTIMIZE_WITH_UV = true;
 
     var canvas = document.getElementById("renderCanvas");
     var engine = new BABYLON.Engine(canvas, true);
     var scene = new BABYLON.Scene(engine);
 
     var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 100, BABYLON.Vector3.Zero(), scene);
-
-    camera.setPosition(new BABYLON.Vector3(-40, 40, 0));
+    camera.setPosition(new BABYLON.Vector3(25, 25, 25));
+    scene.addCamera(camera);
 
     var light = new BABYLON.PointLight("Moon", new BABYLON.Vector3(20, 50, 70), scene);
     var shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
+
+    scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
 
     var beforeRenderFunction = function () {
         // Camera
@@ -35,7 +37,22 @@ window.onload = function init() {
 
     // CALLS
     mapInit(scene);
-    initCharacter(scene, shadowGenerator);
+    initCharacter(scene, camera, shadowGenerator);
+
+    document.addEventListener('keydown', function(event) {
+        if(event.keyCode == 65) {
+            scene.meshes[1].locallyTranslate(new BABYLON.Vector3(1, 0, 0));
+        }
+        else if(event.keyCode == 68) {
+            scene.meshes[1].locallyTranslate(new BABYLON.Vector3(-1, 0, 0));
+        }
+        else if(event.keyCode == 87) {
+            scene.meshes[1].locallyTranslate(new BABYLON.Vector3(0, 0, -1));
+        }
+        else if(event.keyCode == 83) {
+            scene.meshes[1].locallyTranslate(new BABYLON.Vector3(0, 0, 1));
+        }
+    });
 
     engine.runRenderLoop(function () {
         scene.render();
