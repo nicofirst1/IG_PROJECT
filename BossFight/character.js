09b37bc6d7dfffecad4fe0d99fc7568f649b83cc
Eric Stefan Miele <ericstefan.miele@gmail.com>
2018-06-09 17:50:00 +0200

    jump and skybox fix

--- BossFight/character.js
+++ BossFight/character.js
@@ -4,245 +4,51 @@ var jumpBool = true;
 
 var initCharacter = function (scene, camera, shadowGenerator) {
 
-    // var warrior = 0;
-    // BABYLON.SceneLoader.ImportMesh("", "Models/psc-warrior-babylon/", "psc-warrior.babylon", scene, function (newMeshes) {
-    //     warrior = newMeshes[0];
-    //     camera.target = warrior;
-    //
-    //     warrior.scaling = new BABYLON.Vector3(3, 3, 3);
-    //     warrior.rotation.y = Math.PI;
-    //     warrior.position = new BABYLON.Vector3(0, 30, 0);
-    //
-    //     // shadowGenerator.getShadowMap().renderList.push(warrior);
-    //
-    // });
-
-    BABYLON.SceneLoader.ImportMesh("", "Models/dummy/", "dummy3.babylon", scene, function (newMeshes, particleSystems, skeletons) {
-        var skeleton = skeletons[0];
-
-        var warrior_mesh = newMeshes[1];
-
-        skeleton.checkCollisions = true;
-        warrior_mesh.checkCollisions = true;
-
-        camera.target = warrior_mesh;
-        camera.heightOffset = 0;
-        camera.radius = 100;
-
-        warrior_mesh.position.y = 100;
-        var scaling = 11;
-        warrior_mesh.scaling = new BABYLON.Vector3(scaling, scaling, scaling);
-
-
-        shadowGenerator.addShadowCaster(scene.meshes[0], true);
-        for (var index = 0; index < newMeshes.length; index++) {
-            // newMeshes[index].setPivotMatrix(BABYLON.Matrix.Translation(0, 2.35, 0));
-            newMeshes[index].receiveShadows = true;
-
-            newMeshes[index].physicsImpostor = new BABYLON.PhysicsImpostor(newMeshes[index],
-                BABYLON.PhysicsImpostor.CylinderImpostor,
-                {mass: 3, restitution: 0.1, friction: 0.05}, scene);
-
-        }
-
-        // Moves only the skin, not the core body
-        newMeshes[0].position.y = -0.87;
-
-        // ROBOT
-        skeleton.animationPropertiesOverride = new BABYLON.AnimationPropertiesOverride();
-        skeleton.animationPropertiesOverride.enableBlending = true;
-        skeleton.animationPropertiesOverride.blendingSpeed = 0.05;
-        skeleton.animationPropertiesOverride.loopMode = 1;
-
-        var idleRange = skeleton.getAnimationRange("YBot_Idle");
-        var walkRange = skeleton.getAnimationRange("YBot_Walk");
-        var runRange = skeleton.getAnimationRange("YBot_Run");
-        var leftRange = skeleton.getAnimationRange("YBot_LeftStrafeWalk");
-        var rightRange = skeleton.getAnimationRange("YBot_RightStrafeWalk");
-
-        // IDLE
-        if (idleRange) scene.beginAnimation(skeleton, idleRange.from, idleRange.to, true);
-
-        document.addEventListener('keydown', function(event) {
-                if(event.keyCode == 65) {
-                    // Left
-                    if (window.moveW && !window.moveA) {
-                        window.moveA = true;
-                        window.moveD = false;
-                        scene.stopAnimation(skeleton);
-                        var walkAnim = scene.beginWeightedAnimation(skeleton, walkRange.from, walkRange.to, 0.5, true);
-                        var leftAnim = scene.beginWeightedAnimation(skeleton, leftRange.from, leftRange.to, 0.5, true);
-
-                        // Note: Sync Speed Ratio With Master Walk Animation
-                        walkAnim.syncWith(null);
-                        leftAnim.syncWith(walkAnim);
-                    }
-
-                    else if (window.moveS && !window.moveA) {
-                        window.moveA = true;
-                        window.moveD = false;
-                        scene.stopAnimation(skeleton);
-                        var walkAnim = scene.beginWeightedAnimation(skeleton, walkRange.to, walkRange.from, 0.5, true);
-                        var leftAnim = scene.beginWeightedAnimation(skeleton, leftRange.from, leftRange.to, 0.5, true);
-
-                        // Note: Sync Speed Ratio With Master Walk Animation
-                        walkAnim.syncWith(null);
-                        leftAnim.syncWith(walkAnim);
-                    }
-
-                    else if (!window.moveA) {
-                        window.moveA = true;
-                        if (leftRange) scene.beginAnimation(skeleton, leftRange.from, leftRange.to, true);
-                    }
-                }
-                else if(event.keyCode == 68) {
-                    // Right
-                    if (window.moveW && !window.moveD) {
-                        window.moveD = true;
-                        window.moveA = false;
-                        scene.stopAnimation(skeleton);
-                        var walkAnim = scene.beginWeightedAnimation(skeleton, walkRange.from, walkRange.to, 0.5, true);
-                        var rightAnim = scene.beginWeightedAnimation(skeleton, rightRange.from, rightRange.to, 0.5, true);
-
-                        // Note: Sync Speed Ratio With Master Walk Animation
-                        walkAnim.syncWith(null);
-                        rightAnim.syncWith(walkAnim);
-                    }
-
-                    else if (window.moveS && !window.moveD) {
-                        window.moveD = true;
-                        window.moveA = false;
-                        scene.stopAnimation(skeleton);
-                        var walkAnim = scene.beginWeightedAnimation(skeleton, walkRange.to, walkRange.from, 0.5, true);
-                        var leftAnim = scene.beginWeightedAnimation(skeleton, rightRange.from, rightRange.to, 0.5, true);
-
-                        // Note: Sync Speed Ratio With Master Walk Animation
-                        walkAnim.syncWith(null);
-                        leftAnim.syncWith(walkAnim);
-                    }
-
-                    else if (!window.moveD) {
-                        window.moveD = true;
-                        if (rightRange) scene.beginAnimation(skeleton, rightRange.from, rightRange.to, true);
-                    }
-                }
-                else if(event.keyCode == 87) {
-                    // Forward
-                    if (window.moveA && !window.moveW) {
-                        window.moveW = true;
-                        scene.stopAnimation(skeleton);
-                        var walkAnim = scene.beginWeightedAnimation(skeleton, walkRange.from, walkRange.to, 0.5, true);
-                        var leftAnim = scene.beginWeightedAnimation(skeleton, leftRange.from, leftRange.to, 0.5, true);
-
-                        // Note: Sync Speed Ratio With Master Walk Animation
-                        walkAnim.syncWith(null);
-                        leftAnim.syncWith(walkAnim);
-                    }
-                    else if (window.moveD && !window.moveW) {
-                        window.moveW = true;
-                        scene.stopAnimation(skeleton);
-                        var walkAnim = scene.beginWeightedAnimation(skeleton, walkRange.from, walkRange.to, 0.5, true);
-                        var rightAnim = scene.beginWeightedAnimation(skeleton, rightRange.from, rightRange.to, 0.5, true);
-
-                        // Note: Sync Speed Ratio With Master Walk Animation
-                        walkAnim.syncWith(null);
-                        rightAnim.syncWith(walkAnim);
-                    }
-                    else if (!window.moveW) {
-                        window.moveW = true;
-                        if (walkRange) scene.beginAnimation(skeleton, walkRange.from, walkRange.to, true);
-                    }
-                }
-                else if(event.keyCode == 83) {
-                    // Backward
-                    if (window.moveA && !window.moveS) {
-                        window.moveS = true;
-                        scene.stopAnimation(skeleton);
-                        var walkAnim = scene.beginWeightedAnimation(skeleton, walkRange.to, walkRange.from, 0.5, true);
-                        var leftAnim = scene.beginWeightedAnimation(skeleton, leftRange.from, leftRange.to, 0.5, true);
-
-                        // Note: Sync Speed Ratio With Master Walk Animation
-                        walkAnim.syncWith(null);
-                        leftAnim.syncWith(walkAnim);
-                    }
-                    else if (window.moveD && !window.moveS) {
-                        window.moveS = true;
-                        scene.stopAnimation(skeleton);
-                        var walkAnim = scene.beginWeightedAnimation(skeleton, walkRange.to, walkRange.from, 0.5, true);
-                        var rightAnim = scene.beginWeightedAnimation(skeleton, rightRange.from, rightRange.to, 0.5, true);
-
-                        // Note: Sync Speed Ratio With Master Walk Animation
-                        walkAnim.syncWith(null);
-                        rightAnim.syncWith(walkAnim);
-                    }
-                    else if (!window.moveS) {
-                        window.moveS = true;
-                        if (walkRange) scene.beginAnimation(skeleton, walkRange.to, walkRange.from, true);
-                    }
-                }
-                else if(event.keyCode == 32) {
-                    if (!window.jump) {
-                        window.jump = true;
-                    }
-                }
-            });
-
-        document.addEventListener('keyup', function(event) {
+    document.addEventListener('keydown', function(event) {
             if(event.keyCode == 65) {
                 // Left
-                window.moveA = false;
-                if (window.moveW) {
-                    if (walkRange) scene.beginAnimation(skeleton, walkRange.from, walkRange.to, true);
-                }
-                else if (window.moveS) {
-                    if (walkRange) scene.beginAnimation(skeleton, walkRange.to, walkRange.from, true);
-                }
-                else {
-                    if (idleRange) scene.beginAnimation(skeleton, idleRange.from, idleRange.to, true);
-                }
+                window.moveA = true;
             }
             else if(event.keyCode == 68) {
                 // Right
-                window.moveD = false;
-                if (window.moveW) {
-                    if (walkRange) scene.beginAnimation(skeleton, walkRange.from, walkRange.to, true);
-                }
-                else if (window.moveS) {
-                    if (walkRange) scene.beginAnimation(skeleton, walkRange.to, walkRange.from, true);
-                }
-                else {
-                    if (idleRange) scene.beginAnimation(skeleton, idleRange.from, idleRange.to, true);
-                }
+                window.moveD = true;
+
             }
             else if(event.keyCode == 87) {
                 // Forward
-                window.moveW = false;
-                if (!window.moveA && !window.moveD) {
-                    if (idleRange) scene.beginAnimation(skeleton, idleRange.from, idleRange.to, true);
-                }
-                else if (window.moveA) {
-                    if (leftRange) scene.beginAnimation(skeleton, leftRange.from, leftRange.to, true);
-                }
-                else if (window.moveD) {
-                    if (rightRange) scene.beginAnimation(skeleton, rightRange.from, rightRange.to, true);
-                }
+                window.moveW = true;
+
             }
             else if(event.keyCode == 83) {
                 // Backward
-                // Forward
-                window.moveS = false;
-                if (!window.moveA && !window.moveD) {
-                    if (idleRange) scene.beginAnimation(skeleton, idleRange.from, idleRange.to, true);
-                }
-                else if (window.moveA) {
-                    if (leftRange) scene.beginAnimation(skeleton, leftRange.from, leftRange.to, true);
-                }
-                else if (window.moveD) {
-                    if (rightRange) scene.beginAnimation(skeleton, rightRange.from, rightRange.to, true);
+                window.moveS = true;
+            }
+            else if(event.keyCode == 32) {
+                if (!window.jump) {
+                    window.jump = true;
                 }
             }
         });
 
+    document.addEventListener('keyup', function(event) {
+        if (event.keyCode == 65) {
+            // Left
+            window.moveA = false;
+        }
+        else if (event.keyCode == 68) {
+            // Right
+            window.moveD = false;
+
+        }
+        else if (event.keyCode == 87) {
+            // Forward
+            window.moveW = false;
+
+        }
+        else if (event.keyCode == 83) {
+            // Backward
+            window.moveS = false;
+        }
     });
 
 };
--- BossFight/main.js
+++ BossFight/main.js
@@ -1,15 +1,12 @@
-var light_position1=new BABYLON.Vector3(100, 100, 100);
-var light_position2=new BABYLON.Vector3(-100, 100, -100);
-var light_position3=new BABYLON.Vector3(-100, 100, 100);
-var light_position4=new BABYLON.Vector3(100, 100, -100);
+var light_position1 = new BABYLON.Vector3(100, 100, 100);
 
-
-var warrior;
 
 var maxWalkingVelocity = 0.25;
 var startWalkingVelocity = 0.01;
 var currentWalkingVelocity = startWalkingVelocity;
 
+var jumpProgress = 0;
+var maxJump = 20;
 
 window.onload = function init() {
 
@@ -21,13 +18,18 @@ window.onload = function init() {
     var engine = new BABYLON.Engine(canvas, true);
     var scene = new BABYLON.Scene(engine);
 
-    var camera =InitCamera(scene);
+    scene.gravity = new BABYLON.Vector3(0, -0.15, 0);
+    scene.collisionsEnabled = true;
+    scene.enablePhysics();
+
+    var camera = InitCamera(scene);
     var light1 = new BABYLON.PointLight("Moon", light_position1, scene);
 
     // Set full screen
     var setFullScreen = function () {
         engine.isPointerLock = true;
         window.removeEventListener('click', setFullScreen);
+        canvas.requestPointerLock();
     };
     window.addEventListener('click', setFullScreen);
 
@@ -38,7 +40,7 @@ window.onload = function init() {
 
     //----ENGINE
     //disabel online support (remove warnings)
-    engine.enableOfflineSupport=false;
+    engine.enableOfflineSupport = false;
 
     BABYLON.Animation.AllowMatricesInterpolation = true;
 
@@ -49,21 +51,118 @@ window.onload = function init() {
     //add the camera to the scene
     scene.addCamera(camera);
     scene.ambientColor = new BABYLON.Color3(0.3, 0.3, 0.3);
-    //scene.registerBeforeRender(beforeRenderFunction);
+    //scene.registerBeforeRender(beforvar dir = camera.getTarget().subtract(camera.position);
+
+        if (window.moveW) {
+            if (currentWalkingVelocity < maxWalkingVelocity) {
+                currentWalkingVelocity += 0.007;var dir = camera.getTarget().subtract(camera.position);
+
+        if (window.moveW) {
+            if (currentWalkingVelocity < maxWalkingVelocity) {
+                currentWalkingVelocity += 0.007;
+            } else {
+                currentWalkingVelocity = maxWalkingVelocity;
+            }
+            camera.position.x += dir.x;
+            camera.position.y += dir.y;
+            camera.position.z += dir.z;
+        }
+
+        if (window.moveS) {
+            if (currentWalkingVelocity < maxWalkingVelocity) {
+                currentWalkingVelocity += 0.007;
+            } else {
+                currentWalkingVelocity = maxWalkingVelocity;
+            }
+            camera.position.x -= dir.x;
+            camera.position.y -= dir.y;
+            camera.position.z -= dir.z;
+        }
+
+        if (window.moveA) {
+            if (currentWalkingVelocity < maxWalkingVelocity) {
+                currentWalkingVelocity += 0.007;
+            } else {
+                currentWalkingVelocity = maxWalkingVelocity;
+            }
+            var matrix = BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y, Math.PI / 2);
+            var dir2 = BABYLON.Vector3.TransformCoordinates(dir, matrix);
+
+            camera.position.x -= dir2.x;
+            camera.position.y -= dir2.y;
+            camera.position.z -= dir2.z;
+        }
+
+        if (window.moveD) {
+            if (currentWalkingVelocity < maxWalkingVelocity) {
+                currentWalkingVelocity += 0.007;
+            } else {
+                currentWalkingVelocity = maxWalkingVelocity;
+            }
+            var matrix = BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y, Math.PI / 2);
+            var dir2 = BABYLON.Vector3.TransformCoordinates(dir, matrix);
+
+            camera.position.x += dir2.x;
+            camera.position.y += dir2.y;
+            camera.position.z += dir2.z;
+        }
+
+        if (!window.moveW && !window.moveS && !window.moveA && !window.moveD) {
+            currentWalkingVelocity = startWalkingVelocity;
+        }
+            } else {
+                currentWalkingVelocity = maxWalkingVelocity;
+            }
+            camera.position.x += dir.x;
+            camera.position.y += dir.y;
+            camera.position.z += dir.z;
+        }
+
+        if (window.moveS) {
+            if (currentWalkingVelocity < maxWalkingVelocity) {
+                currentWalkingVelocity += 0.007;
+            } else {
+                currentWalkingVelocity = maxWalkingVelocity;
+            }
+            camera.position.x -= dir.x;
+            camera.position.y -= dir.y;
+            camera.position.z -= dir.z;
+        }
+
+        if (window.moveA) {
+            if (currentWalkingVelocity < maxWalkingVelocity) {
+                currentWalkingVelocity += 0.007;
+            } else {
+                currentWalkingVelocity = maxWalkingVelocity;
+            }
+            var matrix = BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y, Math.PI / 2);
+            var dir2 = BABYLON.Vector3.TransformCoordinates(dir, matrix);
+
+            camera.position.x -= dir2.x;
+            camera.position.y -= dir2.y;
+            camera.position.z -= dir2.z;
+        }
+
+        if (window.moveD) {
+            if (currentWalkingVelocity < maxWalkingVelocity) {
+                currentWalkingVelocity += 0.007;
+            } else {
+                currentWalkingVelocity = maxWalkingVelocity;
+            }
+            var matrix = BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y, Math.PI / 2);
+            var dir2 = BABYLON.Vector3.TransformCoordinates(dir, matrix);
+
+            camera.position.x += dir2.x;
+            camera.position.y += dir2.y;
+            camera.position.z += dir2.z;
+        }
+
+        if (!window.moveW && !window.moveS && !window.moveA && !window.moveD) {
+            currentWalkingVelocity = startWalkingVelocity;
+        }
 
     var ground = mapInit(scene);
 
-    scene.enablePhysics();
-
-
-    scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
-    camera.applyGravity = true;
-    camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
-
-    scene.collisionsEnabled = true;
-    camera.checkCollisions = true;
-
-    ground.checkCollisions = true;
     //box.checkCollisions = true;
 
     //initWeapon();
@@ -73,59 +172,79 @@ window.onload = function init() {
     //var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
     //initCharacter(scene, camera, shadowGenerator);
 
-    warrior = scene.meshes[1];
+    //camera = BABYLON.Mesh.CreateSphere("sphere1", 16, 1, scene);
+    //camera.position.y = 15;
 
-    //warrior = BABYLON.Mesh.CreateSphere("sphere1", 16, 1, scene);
-    //warrior.position.y = 15;
+    //camera.physicsImpostor = new BABYLON.PhysicsImpostor(camera, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 10, restitution: 0.9, friction: 0.05 }, scene);
 
-    //warrior.physicsImpostor = new BABYLON.PhysicsImpostor(warrior, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 10, restitution: 0.9, friction: 0.05 }, scene);
-
+    initCharacter();
 
     engine.runRenderLoop(function () {
 
-        if (window.moveW) {
-            if (currentWalkingVelocity < maxWalkingVelocity) {
-                currentWalkingVelocity += 0.007;
-            } else {
-                currentWalkingVelocity = maxWalkingVelocity;
-            }
-            warrior.locallyTranslate(new BABYLON.Vector3(0, 0, -currentWalkingVelocity));
-        }
-
-        if (window.moveS) {
-            if (currentWalkingVelocity < maxWalkingVelocity) {
-                currentWalkingVelocity += 0.007;
-            } else {
-                currentWalkingVelocity = maxWalkingVelocity;
-            }
-            warrior.locallyTranslate(new BABYLON.Vector3(0, 0, currentWalkingVelocity));
-        }
-
-        if (window.moveA) {
-            if (currentWalkingVelocity < maxWalkingVelocity) {
-                currentWalkingVelocity += 0.007;
-            } else {
-                currentWalkingVelocity = maxWalkingVelocity;
-            }
-            warrior.locallyTranslate(new BABYLON.Vector3(currentWalkingVelocity, 0, 0));
-        }
-
-        if (window.moveD) {
-            if (currentWalkingVelocity < maxWalkingVelocity) {
-                currentWalkingVelocity += 0.007;
-            } else {
-                currentWalkingVelocity = maxWalkingVelocity;
-            }
-            warrior.locallyTranslate(new BABYLON.Vector3(-currentWalkingVelocity, 0, 0));
-        }
-
-        if (!window.moveW && !window.moveS && !window.moveA && !window.moveD) {
-            currentWalkingVelocity = startWalkingVelocity;
-        }
+        // var dir = camera.getTarget().subtract(camera.position);
+        //
+        // if (window.moveW) {
+        //     if (currentWalkingVelocity < maxWalkingVelocity) {
+        //         currentWalkingVelocity += 0.007;
+        //     } else {
+        //         currentWalkingVelocity = maxWalkingVelocity;
+        //     }
+        //     camera.position.x += dir.x;
+        //     camera.position.y += dir.y;
+        //     camera.position.z += dir.z;
+        // }
+        //
+        // if (window.moveS) {
+        //     if (currentWalkingVelocity < maxWalkingVelocity) {
+        //         currentWalkingVelocity += 0.007;
+        //     } else {
+        //         currentWalkingVelocity = maxWalkingVelocity;
+        //     }
+        //     camera.position.x -= dir.x;
+        //     camera.position.y -= dir.y;
+        //     camera.position.z -= dir.z;
+        // }
+        //
+        // if (window.moveA) {
+        //     if (currentWalkingVelocity < maxWalkingVelocity) {
+        //         currentWalkingVelocity += 0.007;
+        //     } else {
+        //         currentWalkingVelocity = maxWalkingVelocity;
+        //     }
+        //     var matrix = BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y, Math.PI / 2);
+        //     var dir2 = BABYLON.Vector3.TransformCoordinates(dir, matrix);
+        //
+        //     camera.position.x -= dir2.x;
+        //     camera.position.y -= dir2.y;
+        //     camera.position.z -= dir2.z;
+        // }
+        //
+        // if (window.moveD) {
+        //     if (currentWalkingVelocity < maxWalkingVelocity) {
+        //         currentWalkingVelocity += 0.007;
+        //     } else {
+        //         currentWalkingVelocity = maxWalkingVelocity;
+        //     }
+        //     var matrix = BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y, Math.PI / 2);
+        //     var dir2 = BABYLON.Vector3.TransformCoordinates(dir, matrix);
+        //
+        //     camera.position.x += dir2.x;
+        //     camera.position.y += dir2.y;
+        //     camera.position.z += dir2.z;
+        // }
+        //
+        // if (!window.moveW && !window.moveS && !window.moveA && !window.moveD) {
+        //     currentWalkingVelocity = startWalkingVelocity;
+        // }
 
         if (window.jump) {
-            warrior.locallyTranslate(new BABYLON.Vector3(0, 3, 0));
-            window.jump = false;
+            if (jumpProgress < maxJump) {
+                camera.position.y += 0.3;
+                jumpProgress += 1;
+            } else {
+                jumpProgress = 0;
+                window.jump = false;
+            }
         }
 
         scene.render();
