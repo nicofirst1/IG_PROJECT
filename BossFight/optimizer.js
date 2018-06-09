893939833a000b69ce105c18bd3acd97c7cb2c0d
Nick <brandimax2@gmail.com>
2018-06-10 00:33:42 +0200

    add optimizer

---
+++ BossFight/optimizer.js
@@ -0,0 +1,11 @@
+var initOptimizer=function (scene) {
+
+
+    var options = new BABYLON.SceneOptimizerOptions();
+    options.addOptimization(new BABYLON.HardwareScalingOptimization(0, 1));
+
+    // Optimizer
+    var optimizer = new BABYLON.SceneOptimizer(scene, options);
+
+
+}
