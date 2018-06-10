78fcaa26ae1d6d2b5ed3d8d37ff0268fdd704ff0
Nick <brandimax2@gmail.com>
2018-06-10 17:08:38 +0200

    add sub mana/healt bar

--- BossFight/camera.js
+++ BossFight/camera.js
@@ -135,6 +135,18 @@ var healtBar= function (scene,advancedTexture) {
     healt_bar.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
     healt_bar.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
 
+    var healt_bk = new BABYLON.GUI.Rectangle();
+    healt_bk.width = "500px";
+    healt_bk.height = "40px";
+    healt_bk.cornerRadius = 0;
+    healt_bk.color = "gray";
+    healt_bk.thickness = 4;
+    healt_bk.background = "gray";
+    healt_bk.top="10px";
+
+    healt_bk.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+    healt_bk.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+
     var healt_text=new BABYLON.GUI.TextBlock();
     healt_text.text="100/100";
     healt_text.color="white";
@@ -145,7 +157,7 @@ var healtBar= function (scene,advancedTexture) {
     healt_text.textVerticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
     healt_text.paddingLeft="200px";
 
-
+    advancedTexture.addControl(healt_bk);
     advancedTexture.addControl(healt_bar);
     advancedTexture.addControl(healt_text);
 
@@ -167,6 +179,16 @@ var manaBar= function (scene, advancedTexture) {
     mana_bar.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
     mana_bar.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
 
+    var mana_bk = new BABYLON.GUI.Rectangle();
+    mana_bk.width = "500px";
+    mana_bk.height = "40px";
+    mana_bk.cornerRadius = 0;
+    mana_bk.color = "#5e5e5e";
+    mana_bk.thickness = 4;
+    mana_bk.background = "#5e5e5e";
+    mana_bk.top="55px";
+    mana_bk.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+    mana_bk.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
 
     var mana_text=new BABYLON.GUI.TextBlock();
     mana_text.text="100/100";
@@ -177,6 +199,7 @@ var manaBar= function (scene, advancedTexture) {
     mana_text.paddingLeft="200px";
     mana_text.paddingTop="60";
 
+    advancedTexture.addControl(mana_bk);
     advancedTexture.addControl(mana_bar);
     advancedTexture.addControl(mana_text);
 
