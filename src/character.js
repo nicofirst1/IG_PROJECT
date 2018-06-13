099776fa2d05dd3a61a37321cb060fd1ff4a1ec9
Nick <brandimax2@gmail.com>
2018-06-13 22:38:06 +0200

    moved import model into custom function

--- src/character.js
+++ src/character.js
@@ -30,63 +30,7 @@ var bodyMesh;
 
 var initCharacter = function (scene, camera, shadowGenerator, ground) {
 
-    BABYLON.SceneLoader.ImportMesh("", "Models/dude/", "Dude.babylon", scene, function (newMeshes, particleSystems, skeletons) {
-
-        dude = newMeshes[0];
-        skeleton = skeletons[0];
-        dude.scaling = new BABYLON.Vector3(0.02, 0.02, 0.02);
-        dude.position = new BABYLON.Vector3(0, 0, 0);
-        skeleton.position = new BABYLON.Vector3(0, 0, 0);
-        skeleton.scaling = new BABYLON.Vector3(0.02, 0.02, 0.02);
-        initMat = skeleton.bones[7].getLocalMatrix().clone();
-
-
-        bodyMesh = newMeshes[0];
-        bodyMesh.position.y = -2;
-        bodyMesh.scaling = new BABYLON.Vector3(0.05, 0.05, 0.05);
-        bodyMesh.parent = camera;
-
-        body = skeleton.bones[0];
-        head = skeleton.bones[7];
-
-        upperArmRight = skeleton.bones[12];
-        lowerArmRight = skeleton.bones[14];
-        upperArmLeft = skeleton.bones[31];
-        lowerArmLeft = skeleton.bones[33];
-
-        upperLegRight = skeleton.bones[50];
-        lowerLegRight = skeleton.bones[51];
-        upperLegLeft = skeleton.bones[54];
-        lowerLegLeft = skeleton.bones[55];
-
-        upperLegRight.rotate(BABYLON.Axis.Y, -0.15);
-        upperLegLeft.rotate(BABYLON.Axis.Y, 0.15);
-
-
-        upperArmRight.rotate(BABYLON.Axis.Y, -0.7);
-        upperArmLeft.rotate(BABYLON.Axis.Y, 0.7);
-
-        lowerArmRight.rotate(BABYLON.Axis.Z, -1.5);
-        lowerArmLeft.rotate(BABYLON.Axis.Z, -1.5);
-
-
-        lowerArmRight.rotate(BABYLON.Axis.X, 0.3);
-        lowerArmLeft.rotate(BABYLON.Axis.X, -0.3);
-
-        upperArmRight.translate(new BABYLON.Vector3(3, 0, 4));
-        upperArmLeft.translate(new BABYLON.Vector3(3, 0, -4));
-
-        upperLegRightInit = upperLegRight.getRotation();
-        upperLegLeftInit = upperLegLeft.getRotation();
-        lowerLegRightInit = lowerLegRight.getRotation();
-        lowerLegLeftInit = lowerLegLeft.getRotation();
-
-        upperArmRightInit = upperArmRight.getRotation();
-        upperArmLeftInit = upperArmLeft.getRotation();
-        lowerArmRightInit = lowerArmRight.getRotation();
-        lowerArmLeftInit = lowerArmLeft.getRotation();
-
-    });
+    BABYLON.SceneLoader.ImportMesh("", "Models/dude/", "Dude.babylon", scene,importModel );
 
     document.body.onmousedown = function onClickLeft(event) {
 
@@ -129,8 +73,63 @@ var initCharacter = function (scene, camera, shadowGenerator, ground) {
 
     };
 
+};
+
+var importModel=function  (newMeshes, particleSystems, skeletons)  {
+
+    dude = newMeshes[0];
+    skeleton = skeletons[0];
+    dude.scaling = new BABYLON.Vector3(0.02, 0.02, 0.02);
+    dude.position = new BABYLON.Vector3(0, 0, 0);
+    skeleton.position = new BABYLON.Vector3(0, 0, 0);
+    skeleton.scaling = new BABYLON.Vector3(0.02, 0.02, 0.02);
+    initMat = skeleton.bones[7].getLocalMatrix().clone();
+
+
+    bodyMesh = newMeshes[0];
+    bodyMesh.position.y = -2;
+    bodyMesh.scaling = new BABYLON.Vector3(0.05, 0.05, 0.05);
+    bodyMesh.parent = camera;
+
+    body = skeleton.bones[0];
+    head = skeleton.bones[7];
+
+    upperArmRight = skeleton.bones[12];
+    lowerArmRight = skeleton.bones[14];
+    upperArmLeft = skeleton.bones[31];
+    lowerArmLeft = skeleton.bones[33];
+
+    upperLegRight = skeleton.bones[50];
+    lowerLegRight = skeleton.bones[51];
+    upperLegLeft = skeleton.bones[54];
+    lowerLegLeft = skeleton.bones[55];
+
+    upperLegRight.rotate(BABYLON.Axis.Y, -0.15);
+    upperLegLeft.rotate(BABYLON.Axis.Y, 0.15);
+
+
+    upperArmRight.rotate(BABYLON.Axis.Y, -0.7);
+    upperArmLeft.rotate(BABYLON.Axis.Y, 0.7);
+
+    lowerArmRight.rotate(BABYLON.Axis.Z, -1.5);
+    lowerArmLeft.rotate(BABYLON.Axis.Z, -1.5);
+
+
+    lowerArmRight.rotate(BABYLON.Axis.X, 0.3);
+    lowerArmLeft.rotate(BABYLON.Axis.X, -0.3);
+
+    upperArmRight.translate(new BABYLON.Vector3(3, 0, 4));
+    upperArmLeft.translate(new BABYLON.Vector3(3, 0, -4));
+
+    upperLegRightInit = upperLegRight.getRotation();
+    upperLegLeftInit = upperLegLeft.getRotation();
+    lowerLegRightInit = lowerLegRight.getRotation();
+    lowerLegLeftInit = lowerLegLeft.getRotation();
+
+    upperArmRightInit = upperArmRight.getRotation();
+    upperArmLeftInit = upperArmLeft.getRotation();
+    lowerArmRightInit = lowerArmRight.getRotation();
+    lowerArmLeftInit = lowerArmLeft.getRotation();
+
 }
-
-
-
 
